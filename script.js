49d829686325736f1ee25c0fa76bcd93e4c0fb3e
KHUSHI CHAUDHARY <khushichaudhary904@gmail.com>
2024-06-30 13:05:34 +0530

    30-06-2024

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
 let userScore = 0;
 let compScore = 0;
 
-let choices =  document.querySelectorAll(".choice");
+let choices = document.querySelectorAll(".choice");
 let printResult = document.querySelector(".printMsg");
-const genCompChoice =()=>{
-    const options = ["roock","paper","scissors"];
-    const ramdomIdx= Math.floor(Math.random()*3);
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const ramdomIdx = Math.floor(Math.random() * 3);
     return options[ramdomIdx];
 }
-const playgame =(userChoice)=>{
-    console.log("user choice = ",userChoice);
+const playgame = (userChoice) => {
+    console.log("user choice = ", userChoice);
     const compChoice = genCompChoice();
-    console.log("comp choice = ",compChoice);
-    if(userChoice===compChoice){
-        printResult.innerText = "It's a Draw";
-
+    console.log("comp choice = ", compChoice);
+    if (userChoice === compChoice) {
+        printResult.innerHTML = "It's a Draw";
+        return;
     }
- }
-choices.forEach((choice)=>{
-    let userChoice = choice.getAttribute("id");
-    choice.addEventListener("click",()=>{
+    let userWin = true;
+    if (userChoice == "rock") {
+        userWin = compChoice == "paper" ? false : true;
+    }
+    else if (userChoice == "paper") {
+        userWin = compChoice == "scissors" ? false : true;
+    }
+    else if (userChoice == "scissor") {
+        userWin = compChoice == "rock" ? false : true;
+    }
+    else{
+        userWin=false;
+    }
+    if(userWin){
+        printResult.innerHTML = "I Win";
+        userScore++;
+        document.getElementById("yourScore").innerHTML = userScore;
+    }
+    else {
+        printResult.innerHTML = "Computer Win";
+        compScore++;
+        document.getElementById("compScore").innerHTML = compScore;
+    }
     
+ }
+choices.forEach((choice) => {
+    let userChoice = choice.getAttribute("id");
+    choice.addEventListener("click", () => {
+
         playgame(userChoice);
     })
-    
+
 })
 
 
